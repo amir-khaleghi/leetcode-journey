80a753394b1f5b9dfd2480d07a6f3144b2525364
amir-khaleghi <a.khaleghi.d@gmail.com>
2024-04-07 23:57:02 +0330

    [Day-03]:palindrome-optimized

--- Arrays&Hashing/9.palindrome-number.ts
+++ Arrays&Hashing/9.palindrome-number.ts
@@ -5,15 +5,27 @@
  */
 
 // @lc code=start
+// function isPalindrome(x: number): boolean {
+//   const stringNumber = x.toString();
+//   const reverseString = stringNumber.split('').reverse().join('');
+//   if (stringNumber === reverseString) {
+//     return true;
+//   }
+//   return false;
+// }
 function isPalindrome(x: number): boolean {
-  const stringNumber = x.toString();
-  console.log(stringNumber);
-  const reverseString = stringNumber.split('').reverse().join('');
-  console.log(reverseString);
-  if (stringNumber === reverseString) {
-    return true;
+  if (x < 0) {
+    return false; // Negative numbers can't be palindromes
   }
-  return false;
+
+  const stringNumber = x.toString();
+  const length = stringNumber.length;
+  for (let i = 0; i < length / 2; i++) {
+    if (stringNumber[i] !== stringNumber[length - 1 - i]) {
+      return false; // If characters don't match, it's not a palindrome
+    }
+  }
+  return true;
 }
 // @lc code=end
 isPalindrome(121);
